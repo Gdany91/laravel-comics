6dac49f60a4ff7d6ab6b9e00b28c4f02e4a625a7
gdany91 <you@example.comdgarofalo52@gmail.com>
2023-01-30 19:51:09 +0100

    footer (piu o meno)

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,45 @@
+
+    <div class="container-fluid  my_bg">
+        <div class="container col-8 d-flex footer_container" >
+            <div class="list_menu_footer d-flex  ">
+                <ul class="items">
+                    <li><h3>DC COMICS</h3></li>
+                    <li>rger</li>
+                    <li>fref</li>
+                    <li>ferf</li>
+                    <li>fref</li>
+                    <li>erf</li>
+                    <li>fre</li>
+                    <li>gtt</li>
+                </ul>
+
+                <ul class="items">
+                    <li><h3>DC COMICS</h3></li>
+                    <li>rger</li>
+                    <li>fref</li>
+                    <li>ferf</li>
+                    <li>fref</li>
+                    <li>erf</li>
+                    <li>fre</li>
+                    <li>gtt</li>
+                </ul>
+
+                <ul class="items">
+                    <li><h3>DC COMICS</h3></li>
+                    <li>rger</li>
+                    <li>fref</li>
+                    <li>ferf</li>
+                    <li>fref</li>
+                    <li>erf</li>
+                    <li>fre</li>
+                    <li>gtt</li>
+                </ul>
+
+            </div>
+
+            <div class="big_logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+            </div>
+          
+        </div>
+    </div>
